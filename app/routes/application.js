2065b2e62048977569f132e5cde4acac069d74e8
TinaLoh <tinalola@gmail.com>
2015-02-26 22:26:50 -0800

    changed css and alignment

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
+
     search: function() {
       var searchTerm = this.controllerFor("application").get("search");
       this.transitionTo("search", searchTerm);
